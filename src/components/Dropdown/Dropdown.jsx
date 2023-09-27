d4a481186aadec89d6c2c25b16caf0ad0611defd
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-09-27 13:41:42 +0200

    Correctifs logo, animation home card, début dropdown, ajout fontawesome

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,14 @@
+import "./Dropdown.scss";
+
+function Dropdown() {
+    return (
+        <div>
+            <i class="fa-solid fa-chevron-down"></i>
+            <i class="fa-solid fa-chevron-up"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-circle"></i>
+        </div>
+    );
+}
+
+export default Dropdown;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,13 +2,14 @@ import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import imageAboutBanner from "../../assets/images/about_banner.jpg";
 import Footer from "../../components/Footer/Footer";
+import Dropdown from "../../components/Dropdown/Dropdown";
 
 function About() {
     return (
         <div>
             <Header />
             <Banner image={imageAboutBanner} />
-            <h1> Page A propos 🏠</h1>
+            <Dropdown />
             <Footer />
         </div>
     );
