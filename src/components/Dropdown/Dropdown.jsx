6f0442c3355983a622abdede946de6d2a8e350da
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-09-29 16:26:16 +0200

    Importation librairie font, création composant dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,14 +1,30 @@
 import "./Dropdown.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-function Dropdown() {
+function Dropdown({ title, children }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
-        <div>
-            <i class="fa-solid fa-chevron-down"></i>
-            <i class="fa-solid fa-chevron-up"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-circle"></i>
-        </div>
+        <section className="dropdown">
+            <div className="dropdown_top">
+                {title && <h2>{title}</h2>}
+                <button className="dropdown_top_button" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
+                </button>
+            </div>
+            {isOpen && children}
+        </section>
     );
 }
 
 export default Dropdown;
+
+/* <i class="fa-solid fa-chevron-down"></i>
+            <i class="fa-solid fa-chevron-up"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-circle"></i> */
